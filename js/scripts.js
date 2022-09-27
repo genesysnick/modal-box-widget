848f2ba6d1dab7076a248b99b4d7802a51b700d0
Erwin Merioles <Erwin@Merioles.net>
2022-09-27 20:46:06 +0800

    js/scripts.js - Added intierestCohort fix.

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,29 @@
 
 window.genesys_checker = null;
 
+(function () {
+	const cohorts = "interestCohort";
+	const documentProto = Document.prototype;
+	const flocSupported = cohorts in documentProto;
+
+	if (!flocSupported) {
+		return;
+	}
+
+	const descriptor = Object.getOwnPropertyDescriptor(documentProto, cohorts);
+	const writable = descriptor && descriptor.writable;
+	if (writable) {
+		const proxy = new Proxy(documentProto[cohorts], { apply: () => Promise.reject() });
+		const config = {
+			writable: false,
+			value: proxy,
+			configurable: false,
+			enumerable: false,
+		};
+		Object.defineProperty(documentProto, cohorts, config);
+	}
+})();
+
 $(document).ready( function() {
 	$('#start_chat').click( function() {
 		if ( genesys_checker == null && typeof( Genesys ) == 'undefined' ) {
